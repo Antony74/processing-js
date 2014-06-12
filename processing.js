d1de21dafe6bd45e28e67e66948961d1c63a36f8
Antony74 <akb@akb.me.uk>
2014-06-12 15:08:25 +0100

    applyMatrix #103, fix for PMatrix3D. (presumably an equivalent bug still exists for PMatrix2D, but I'm not going to try and second-guess what the correct fix might be)

--- processing.js
+++ processing.js
@@ -13022,6 +13022,9 @@ module.exports = function setupParser(Processing, options) {
     */
     DrawingShared.prototype.applyMatrix = function() {
       var a = arguments;
+      if (arguments.length === 1 && arguments[0] instanceof PMatrix3D) {
+        a = arguments[0].array();
+      }
       modelView.apply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15]);
       modelViewInv.invApply(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15]);
     };
